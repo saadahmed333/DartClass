6093f75af982b30c0af45ed9144573e932e3fd61
saad <saadahmed1742@gmail.com>
2023-09-16 03:16:16 -0700

    overview of for loop and nested for loop

--- main.dart
+++ main.dart
@@ -14,14 +14,13 @@ var data2 = {"class": "flutter","batch": "2", "time": '2pm'};
 // data2.removeWhere((key, value) => key == "batch");
 // print(data2);
 
-var f1 = {1: "Apple", 2: "banana"};
-var f2 = {3: "Mango"};
-var f3 = {4: "orange"};
+// var f1 = {1: "Apple", 2: "banana"};
+// var f2 = {3: "Mango"};
+// var f3 = {4: "orange"};
 //  spread operator
 // var fruit = {}..addAll(f2)..addAll(f1)..addAll(f3);
-var fruit1 = {...f1, ...f3, ...f2};
-
-print(fruit1);
+// var fruit1 = {...f1, ...f3, ...f2};
+// print(fruit1);
 
 // var listOfKey = ["name", "fname", "age"];
 // var listOfValue = ["Saad Ahmed", "Saleem Ahmed", "19"];
@@ -30,4 +29,17 @@ print(fruit1);
 // print(personalInfo.containsKey("name"));
 // print(personalInfo.containsKey("rollNo"));
 // print(personalInfo.containsValue("Saad Ahmed"));
+
+//   for loop
+// int count = 10;
+// for (int i = 1; i <= 10; i++) {
+//   print("${count} x ${i} = ${count*i}");
+// }
+
+// nested for loop
+for (int i = 2; i <= 10; i++) {
+  for (var j = 1; j <= 10; j++) {
+    print("${i} x ${j} = ${i*j}");
+  }
+}
 }
