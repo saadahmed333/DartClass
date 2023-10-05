f6c031bb22cb84c7376316f2736430b63b185743
saadahmed333 <saadahmed1742@gmail.com>
2023-10-05 13:00:44 -0700

    assignment 2 question(1)

---
+++ assignment.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+  // ie: if both values are equal then it's square otherwise rectangle.
+  var heigth = 2;
+  var width = 2;
+  if (heigth == width) {
+    print("it's square");
+  } else {
+    print("it's rectangle");
+  }
+}
